8781e34e0f2a93e4f180c7866e710066400006ec
Addi Amaya <addi.amaya99@gmail.com>
2021-02-27 18:55:54 -0600

    Got rid of the binning and evaulated using variance

--- image_quality.c
+++ image_quality.c
@@ -21,81 +21,69 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
+//TODO: Can a determination for variance range between 4000-11000, pass those values
 
-#define BINS 5  // number of bins to hold each set of values. 
+void variant(unsigned char *image_buffer, long fileLen){
+   double sum;
+   float X_bar;
+   float variance;
+   for ( int i = 0; i < fileLen; i ++){
+        sum = sum + image_buffer[i];
+    }
+    printf("Sum is: %f\n", sum);
 
+   X_bar = sum/fileLen;
 
-/******* This could be a downside: From calculation limit array values done below, it is discovered that the higher the number of bins, the greater the values displayed. Less bins will display less values. ***********/
+   printf("xbar is: %f \n",X_bar);
 
+    float sum_x_minus_xbar = 0;
 
-void sort_display(unsigned char* buffer, long filelen) {
+    for ( int i = 0; i < fileLen; i ++){
+        sum_x_minus_xbar = sum_x_minus_xbar + ( pow((image_buffer[i] - X_bar),2));
+    }
+    printf("Sum numerator is: %f \n", sum_x_minus_xbar);
 
-/* Functions take an array of bytes (char datatype) and the filelen. Each char represent a value from 0 to 255. Chars will be sorted into bins numerically.*/
+    variance = sum_x_minus_xbar /(fileLen-1);
 
-
-/* Values are represented as unsigned characters ranging 0 to 255. */
-
-       unsigned char one_bin = 255/BINS;               // split bins into limit for one bin
-
-        int limit[BINS];                      // create array for all the limits
-
-        for (int m=0; m<BINS; m++) {
-            
-                limit[m] = -128 + (one_bin * m) ;        // calculate values for each limit and store in array
-        }
-
-        
-        // For each bin, go through data, sort into array
-        int binArray[BINS];
-
-        for (int k=0; k<BINS; k++) {
-
-            for(int j=0; j<filelen; j++) {
-    
-               if((buffer[j] > limit[k]) && (buffer[j] <= limit[k+1])) {                                   
-                  binArray[BINS]++; 
-               } 
-            }
-        }
-}
-
-void calculate(unsigned char* array){
-
+    printf("Variance: %f\n", variance);
+    printf("S.D: %f\n", sqrt(variance));
+    printf("File length is: %d", fileLen);
 }
 
 
-// int main (int argc, char *argv[]) {
+int main (int argc, char *argv[]) {
 
-// /* takes two command line arguments; the program name and one input file. 
-// Extracts data from file into array. Displays final results. */
+/* takes two command line arguments; the program name and one input file. 
+Extracts data from file into array. Displays final results. */
 
 
-//      if (argc != 2) {
-//         printf("Usage: <exe_name> <image_file>\n");
-//         return 1;
-//         }
+     if (argc != 2) {
+        printf("Usage: <exe_name> <image_file>\n");
+        return 1;
+        }
 
     
    
-//     long filelen;
-//     FILE *fileptr = fopen(argv[1], "rb");  // Open the file in binary mode
+    long filelen;
+    FILE *fileptr = fopen(argv[1], "rb");  // Open the file in binary mode
 
-//     // Make sure file opened successfully.
-//     if(fileptr==NULL) {printf("Unable to open %s for reading.\n", argv[1]); return 1;}
-
-
-//     fseek(fileptr, 0, SEEK_END);          // Jump to the end of the file
-//     filelen = ftell(fileptr);             // Get the current byte offset in the file
-//     rewind(fileptr);                      // Jump back to the beginning of the file
-//     unsigned char *buffer = (unsigned char *)malloc(filelen * sizeof(unsigned char)); // Enough memory for the file
-//     fread(buffer, filelen, 1, fileptr); // Read in the entire file
+    // Make sure file opened successfully.
+    if(fileptr==NULL) {printf("Unable to open %s for reading.\n", argv[1]); return 1;}
 
 
-//     sort_display(buffer, filelen);
+    fseek(fileptr, 0, SEEK_END);          // Jump to the end of the file
+    filelen = ftell(fileptr);             // Get the current byte offset in the file
+    rewind(fileptr);                      // Jump back to the beginning of the file
+    unsigned char *buffer = (unsigned char *)malloc(filelen * sizeof(unsigned char)); // Enough memory for the file
+    fread(buffer, filelen, 1, fileptr);   // Read in the entire file
 
-//     free(buffer);
-//     fclose(fileptr); // Close the file
 
-//     }
+    variant(buffer, filelen);
+
+    free(buffer);
+    fclose(fileptr);
+
+}
 
