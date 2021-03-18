afb5d1149d67e33b5e0da4c0c2d5ba38705bb44a
Addi Amaya <addi.amaya99@gmail.com>
2021-03-17 20:08:23 -0600

    added hex values for telecommands and outline for next week

--- ImagePrepTask.c
+++ ImagePrepTask.c
@@ -12,10 +12,37 @@
 * @return None
 */
 
+//TODO: change type to char
+
 void imagePrepTask(void *size, void *ImageID){
+    long imgDownloadSRAM1TopThumb = 0x1F7F400041FFF;
+    long imgDownloadSRAM1BotThumb = 0x1F7F400141FFF;
+    long imgDownloadSRAM2BotThumb = 0x1F7F401141FFF;
+    long imgDownloadSRAM1TopFull = 0x1F7F400001FFF;
+    long imgDownloadSRAM1BotFull = 0x1F7F400101FFF;
+    long imgDownloadSRAM2BotFull = 0x1F7F401101FFF;
 
+    //Case 1: Thumbnail
+        //send telecommand
+        //check success of telecommand (if failed send again)
+        //Timed feature ^
 
+        //call quality check function
+            
+        //Compress image with library
 
+        //send to downlink people
+
+    //Case 2: Full photo
+        //send telecommand
+        //check success of telecommand (if failed send again)
+        //Timed feature ^
+
+        //call quality check function
+            
+        //Compress image with library
+
+        //send to downlink people
 }
 
 
