5bfe42c0c1d10203e427aafc184154253ea0a667
Addi Amaya <addi.amaya99@gmail.com>
2021-03-17 19:54:06 -0600

    Doxygen and Task outline

---
+++ ImagePrepTask.c
@@ -0,0 +1,23 @@
+/**
+* @brief Image Preparation Task
+*
+*  The image preparation task is responsible for downloading the image taken from the Capture task, 
+*  filtering unwanted photos, and preparing for downlinks.
+*
+* @header software-and-command/main-application
+* @param size determined whether or not the photo to deal with is full or thumbnail
+* @param ImageID is the unique ID of the photo 
+* @pre None
+* @post For full image downlink it will raise SRAM slot flag
+* @return None
+*/
+
+void imagePrepTask(void *size, void *ImageID){
+
+
+
+}
+
+
+
+//Struct for image ID and image
